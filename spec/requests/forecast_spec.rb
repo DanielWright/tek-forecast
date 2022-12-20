c9978f560acc4efdc88c3e254696b7465ac31d4c
Daniel Wright <dan.wright@gmail.com>
2022-12-20 12:25:42 -0800

    Begin Driving Out Invalid-Search Case
    An invalid query will be the motivation to eliminate the hard-coded response
    status and, eventually, body.

--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -1,7 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "A /forecast endpoint" do
-  subject(:forecast!) { get "/forecast" }
+  let(:search_query) { nil }
+
+  subject(:forecast!) { get "/forecast", params: { q: search_query } }
 
   it "responds 200 OK" do
     forecast!
@@ -36,7 +38,19 @@ RSpec.describe "A /forecast endpoint" do
     end
   end
 
-  context "with an invalid search parameter"
+  context "with an invalid search parameter" do
+    let(:search_query) { "gobbledegook" }
+
+    # NOTE: There are reasonable arguments against 400 as the semantically
+    # appropriate response code here, but I'd want to involve stakeholders
+    # in such a decision, and 400 is a valid placeholder for the time being.
+    it "responds 400 Bad Request" do
+      forecast!
+
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
   context "with a valid set of coordinates"
   context "with a valid zipcode"
   context "with a valid address"
